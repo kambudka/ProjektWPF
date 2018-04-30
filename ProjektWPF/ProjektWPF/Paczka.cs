9c6ea18ccf088479c7af4f7e64335466aa9f16a2
kambudka <kambudka@wp.pl>
2018-04-30 13:39:20 +0200

    Pomysł

---
+++ ProjektWPF/ProjektWPF/Paczka.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ProjektWPF
+{
+    public class Paczka
+    {
+        public int Numer {get; set;}
+        public decimal Cena { get; set; }
+        public string Adres { get; set; }
+        public int Waga { get; set; }
+        public string Status { get; set; }
+
+        public string this[string columnname]
+        {
+            get
+            {
+                if (columnname == "Waga")
+                {
+                    if (Waga >= 5)
+                        return "Waga nie moze być większa niż 5 kg.";
+                }
+                return null;
+            }
+        }
+    }
+}
